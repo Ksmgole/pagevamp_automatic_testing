c5bd89248f975089768e5ccb983bf4faedec4dde
Gole G <kusumgole67@gmail.com>
2018-05-11 17:50:44 +0545

    pricing and partners link test

--- tests/acceptance/HomepageCest.php
+++ tests/acceptance/HomepageCest.php
@@ -14,86 +14,86 @@ class HomepageCest
     }
 
 
-//    public function test_logo_click_redirects_to_homepage(AcceptanceTester $I)
-//    {
-//        $I->amOnPage('/');
-//
-//        $I->click(['class' => 'pv-logo']);
-//        $I->amOnPage('/pricing');
-//        $I->click(['class' => 'pv-logo']);
-//        $I->click(['class' => 'pv-footer-logo']);
-//
-//        $I->amOnPage('/?desh=akky');
-//        $I->click(['class' => 'pv-logo--sponsr']);
-//        $I->switchToNextTab();
-//        $I->seeInTitle('Akky :: Inicio minisitio Pagevamp');
-//    }
-//
-//
-//    public function test_italian_policy_links_open_italian_cookie_policy_page(AcceptanceTester $I)
-//    {
-//        $I->amOnPage('/?desh=it');
-//        $I->scrollTo('footer');
-//        $I->click('//a[@href="https://www.pagevamp.com/it/cookie-policy"]');
-//        $I->amOnUrl('https://www.pagevamp.com/it/cookie-policy');
-//
-//
-//        $I->amOnPage('/?desh=it');
-//        $I->see('Questo sito utilizza cookie per il funzionamento e la migliore gestione del sito');
-//        $I->click('Cookie Policy');
-//        $I->wait(5);
-//        $I->amOnUrl('https://www.pagevamp.com/it/cookie-policy');
-//
-//        $I->amOnPage('/?desh=it');
-//        $I->click('Accetto');
-//    }
-//
-//
-//    public function test_scroll_to_how_it_works_and_youtube_video_link(AcceptanceTester $I)
-//    {
-//        $I->amOnPage('/');
-//        $I->click('#how-it-works-link');
-//        $I->wait(2);
-//        $I->click('//a[@data-target="#how-it-works-video"]');
-//        $I->wait(5);
-//        $I->click('#how-it-works-yt-video');
-//
-//        $I->amOnPage('/?desh=it');
-//        $I->scrollTo(['css'=>'.pv-how-it-works']);
-//        $I->dontSee('Watch it in action');
-//    }
-//
-//
-//    public function test_prices_and_links_on_pricing_page(AcceptanceTester $I)
-//    {
-//        $I->amOnPage('/');
-//        $I->click('.navbar-right a[href="https://www.pagevamp.com/np/pricing"]');
-//
-//        $I->scrollTo('footer');
-//        $I->click('.pv-footer-list a[href="https://www.pagevamp.com/np/pricing"]');
-//
-//        $I->amOnPage('/');
-//        $I->scrollTo('.pv-pricing');
-//        $I->see('$ 12');
-//        $I->see('$ 15');
-//        $I->click('.pv-pricing a[href="https://www.pagevamp.com/np/pricing"]');
-//
-//        $I->see('Every plan starts off with a 14-day free trial.');
-//        $I->see('$ 12');
-//        $I->see('$ 15');
-//        $I->click('//a[@href="#ques-2"]');
-//        $I->wait(2);
-//        $I->click('//a[@href="https://pagevamp.zendesk.com/hc/en-us/articles/204463719-Connecting-a-Domain-Purchased-Elsewhere"]');
-//        $I->wait(3);
-//        $I->click('//a[@href="#ques-3"]');
-//        $I->click('//a[@href="https://pagevamp.zendesk.com/hc/en-us/articles/204463719-Connecting-a-Domain-Purchased-Elsewhere"]');
-//        $I->wait(5);
-//
-//        $I->click('//a[@href="#ques-7"]');
-//        $I->click('.btn--green');
-//        $I->wait(5);
-//        $I->click('#login-fb');
-//    }
+    public function test_logo_click_redirects_to_homepage(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+
+        $I->click(['class' => 'pv-logo']);
+        $I->amOnPage('/pricing');
+        $I->click(['class' => 'pv-logo']);
+        $I->click(['class' => 'pv-footer-logo']);
+
+        $I->amOnPage('/?desh=akky');
+        $I->click(['class' => 'pv-logo--sponsr']);
+        $I->switchToNextTab();
+        $I->seeInTitle('Akky :: Inicio minisitio Pagevamp');
+    }
+
+
+    public function test_italian_policy_links_open_italian_cookie_policy_page(AcceptanceTester $I)
+    {
+        $I->amOnPage('/?desh=it');
+        $I->scrollTo('footer');
+        $I->click('//a[@href="https://www.pagevamp.com/it/cookie-policy"]');
+        $I->amOnUrl('https://www.pagevamp.com/it/cookie-policy');
+
+
+        $I->amOnPage('/?desh=it');
+        $I->see('Questo sito utilizza cookie per il funzionamento e la migliore gestione del sito');
+        $I->click('Cookie Policy');
+        $I->wait(5);
+        $I->amOnUrl('https://www.pagevamp.com/it/cookie-policy');
+
+        $I->amOnPage('/?desh=it');
+        $I->click('Accetto');
+    }
+
+
+    public function test_scroll_to_how_it_works_and_youtube_video_link(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->click('#how-it-works-link');
+        $I->wait(2);
+        $I->click('//a[@data-target="#how-it-works-video"]');
+        $I->wait(5);
+        $I->click('#how-it-works-yt-video');
+
+        $I->amOnPage('/?desh=it');
+        $I->scrollTo(['css'=>'.pv-how-it-works']);
+        $I->dontSee('Watch it in action');
+    }
+
+
+    public function test_prices_and_links_on_pricing_page(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->click('.navbar-right a[href="https://www.pagevamp.com/np/pricing"]');
+
+        $I->scrollTo('footer');
+        $I->click('.pv-footer-list a[href="https://www.pagevamp.com/np/pricing"]');
+
+        $I->amOnPage('/');
+        $I->scrollTo('.pv-pricing');
+        $I->see('$ 12');
+        $I->see('$ 15');
+        $I->click('.pv-pricing a[href="https://www.pagevamp.com/np/pricing"]');
+
+        $I->see('Every plan starts off with a 14-day free trial.');
+        $I->see('$ 12');
+        $I->see('$ 15');
+        $I->click('//a[@href="#ques-2"]');
+        $I->wait(2);
+        $I->click('//a[@href="https://pagevamp.zendesk.com/hc/en-us/articles/204463719-Connecting-a-Domain-Purchased-Elsewhere"]');
+        $I->wait(3);
+        $I->click('//a[@href="#ques-3"]');
+        $I->click('//a[@href="https://pagevamp.zendesk.com/hc/en-us/articles/204463719-Connecting-a-Domain-Purchased-Elsewhere"]');
+        $I->wait(5);
+
+        $I->click('//a[@href="#ques-7"]');
+        $I->click('.btn--green');
+        $I->wait(5);
+        $I->click('#login-fb');
+    }
 
 
     public function test_countrywise_partners_typeform(AcceptanceTester $I)
